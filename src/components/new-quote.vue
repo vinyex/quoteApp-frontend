38e7e5075769720ea7be1880708ead759cbb0fa5
vinyex <irvinepramudya@gmail.com>
2018-10-11 18:34:25 +0700

    frontend file quote App

---
+++ src/components/new-quote.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+			<form @submit.prevent="onSubmitted">
+				<div class="form-group">
+					<label for="content">
+						<b>Quote</b>
+					</label>
+					<br/>
+				<i> Write Your Quote</i>
+					<textarea name="" id="content" class="form-control" v-model="quoteContent" cols="80" rows="6"></textarea>
+				</div>
+				<div>
+					<button type="submit" class="btn btn-success">
+						Submit
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</template>
+
+<script type="text/babel">
+	import axios from 'axios';
+	export default {
+		data() {
+			return {
+				quoteContent: ''
+			}
+		},
+		methods: {
+			onSubmitted() {
+				axios.post('http://localhost:8000/api/quote', 
+						{content: this.quoteContent})
+						.then((response) => {
+						window.location.href = "/";
+						})
+			.catch ((error) => console.log(error)
+			)}
+		}
+	}
+</script>
+
+<style scoped>
+	#content {
+		margin-top: 40px;
+	}
+</style>
